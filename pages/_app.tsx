1df189f9a351a1ef3f63d9fd5bb6ff65a3d59261
Devookim <khwoo8026@gmail.com>
2023-04-12 01:01:57 +0900

    move components folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Chatbar from "./components/Chatbar/Chatbar";
+import Chatbar from "../components/Chatbar/Chatbar";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
