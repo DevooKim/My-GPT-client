0eb2cfe00279baf31bcbc992b850945bdcee4bc9
Devookim <khwoo8026@gmail.com>
2023-04-11 17:13:27 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Chatbar from "./components/Chatbar/Chatbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Chatbar />
+            <Component {...pageProps} />
+        </>
+    );
 }
